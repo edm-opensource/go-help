a0614eba3d7f6c9d7ae4a7b43243fcb10a3f47cb
David Josefson <david.josefson@gmail.com>
2015-11-12 15:20:57 +0100

    Added tests for RemoveDuplicateInts

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -2,6 +2,7 @@ package arrays
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -35,6 +36,51 @@ func TestReverseString_3(t *testing.T) {
 			fmt.Sprintf("%v", expected)+", err == <nil>",
 			fmt.Sprintf("%v", actual),
 			fmt.Sprintf("%v", err)))
+	}
+}
+
+/*
+	TestRemoveDuplicates
+	input := []int{1, 1}
+	expected := []int{1}
+*/
+func TestRemoveDuplicates(t *testing.T) {
+	input := []int{1, 1}
+	expected := []int{1}
+	actual := RemoveDuplicateInts(input)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatError(fmt.Sprintf("%v", input), fmt.Sprintf("%v", expected), fmt.Sprintf("%v", actual)))
+	}
+}
+
+/*
+	TestRemoveDuplicates_2
+	input := []int{9, 1, 1, 1, 100, 100, 9, 9}
+	expected := []int{9, 1, 100}
+*/
+func TestRemoveDuplicates_2(t *testing.T) {
+	input := []int{9, 1, 1, 1, 100, 100, 9, 9}
+	expected := []int{9, 1, 100}
+	actual := RemoveDuplicateInts(input)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatError(fmt.Sprintf("%v", input), fmt.Sprintf("%v", expected), fmt.Sprintf("%v", actual)))
+	}
+}
+
+/*
+	TestRemoveDuplicates_3
+	input := []int{}
+	expected := []int{}
+*/
+func TestRemoveDuplicates_3(t *testing.T) {
+	input := []int{}
+	expected := []int{}
+	actual := RemoveDuplicateInts(input)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatError(fmt.Sprintf("%v", input), fmt.Sprintf("%v", expected), fmt.Sprintf("%v", actual)))
 	}
 }
 
