ea1f207d11542394ac3f60682afc3561cd20354b
David Josefson <david.josefson@gmail.com>
2015-11-12 15:20:33 +0100

    Fixed an index bug in the inner for loop

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,13 +1,21 @@
 package arrays
 
-func RemoveDuplicates(slice []int) []int {
+/*
+RemoveDuplicateInts finds and removes any duplicate values for a given int slice
+*/
+func RemoveDuplicateInts(slice []int) []int {
 	lenSlice := len(slice)
+
+	if lenSlice < 2 {
+		return slice
+	}
 
 	for i := 0; i < lenSlice; i++ {
 		for j := i + 1; j < lenSlice; j++ {
 			if slice[i] == slice[j] {
 				slice = append(slice[:j], slice[j+1:]...)
 				lenSlice = len(slice)
+				j--
 			}
 		}
 	}
