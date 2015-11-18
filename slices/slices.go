b35680f36469edaad70ee82ce5f9d633164235ed
Agetorp <elias.agetorp@gmail.com>
2015-11-18 11:10:25 +0100

    Added RemoveAllInstancesOf and Contains for string, int and float64

--- slices/slices.go
+++ slices/slices.go
@@ -76,3 +76,93 @@ func ReverseFloat64s(s []float64) []float64 {
 
 	return res
 }
+
+/*
+RemoveAllInstancesOfStrings returns a copy of the first slice parameter,
+with all instances of the rest of the arguments removed
+*/
+func RemoveAllInstancesOfStrings(s []string, toBeRemoved ...string) []string {
+	length := len(s)
+	var res []string
+
+	for i := 0; i < length; i++ {
+		if !ContainsString(toBeRemoved, s[i]) {
+			res = append(res, s[i])
+		}
+	}
+
+	return res
+}
+
+/*
+RemoveAllInstancesOfInts returns a copy of the first slice parameter,
+with all instances of the rest of the arguments removed
+*/
+func RemoveAllInstancesOfInts(s []int, toBeRemoved ...int) []int {
+	length := len(s)
+	var res []int
+
+	for i := 0; i < length; i++ {
+		if !ContainsInt(toBeRemoved, s[i]) {
+			res = append(res, s[i])
+		}
+	}
+
+	return res
+}
+
+/*
+RemoveAllInstancesOfFloat64s returns a copy of the first slice parameter,
+with all instances of the rest of the arguments removed
+*/
+func RemoveAllInstancesOfFloat64s(s []float64, toBeRemoved ...float64) []float64 {
+	length := len(s)
+	var res []float64
+
+	for i := 0; i < length; i++ {
+		if !ContainsFloat64(toBeRemoved, s[i]) {
+			res = append(res, s[i])
+		}
+	}
+
+	return res
+}
+
+/*
+ContainsString returns true if the slice contains the string
+*/
+func ContainsString(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
+
+/*
+ContainsInt returns true if the slice contains the int
+*/
+func ContainsInt(slice []int, item int) bool {
+	set := make(map[int]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
+
+/*
+ContainsFloat64 returns true if the slice contains the float64
+*/
+func ContainsFloat64(slice []float64, item float64) bool {
+	set := make(map[float64]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
--- slices/slices_test.go
+++ slices/slices_test.go
@@ -177,9 +177,266 @@ func TestRemoveDuplicates_3(t *testing.T) {
 	}
 }
 
+/*
+	TestRemoveAllInstancesOfStrings_1
+	input := []string{"one", "one", "two", "oone"}
+	input2 := []string{"one"}
+	expected := []string{"two", "oone"}
+*/
+func TestRemoveAllInstancesOfStrings_1(t *testing.T) {
+	input := []string{"one", "one", "two", "oone"}
+	input2 := []string{"one"}
+	expected := []string{"two", "oone"}
+	actual := RemoveAllInstancesOfStrings(input, input2...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfStrings_2
+	input := []string{"one", "\none", "two", "oone"}
+	input2 := []string{"one"}
+	expected := []string{"\none", "two", "oone"}
+*/
+func TestRemoveAllInstancesOfStrings_2(t *testing.T) {
+	input := []string{"one", "\none", "two", "oone"}
+	input2 := []string{"one"}
+	expected := []string{"\none", "two", "oone"}
+	actual := RemoveAllInstancesOfStrings(input, input2...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfStrings_3
+	input := []string{"one", "one", "two", "oone"}
+	input2 := []string{""}
+	expected := []string{"one", "one", "two", "oone"}
+*/
+func TestRemoveAllInstancesOfStrings_3(t *testing.T) {
+	input := []string{"one", "one", "two", "oone"}
+	input2 := []string{""}
+	expected := []string{"one", "one", "two", "oone"}
+	actual := RemoveAllInstancesOfStrings(input, input2...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfInts_1
+	input := []int{1, 2, 3, 2, 2, 4, 2, 6, 12}
+	input2 := []int{2, 1}
+	expected := []int{3, 4, 6, 12}
+*/
+func TestRemoveAllInstancesOfInts_1(t *testing.T) {
+	input := []int{1, 2, 3, 2, 2, 4, 2, 6, 12}
+	input2 := []int{2, 1}
+	expected := []int{3, 4, 6, 12}
+	actual := RemoveAllInstancesOfInts(input, input2...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfInts_2
+	input := []int{1, 2, 3, 2, 2, 4, 2, 6, 12}
+	input2 := []int{16}
+	expected := []int{1, 2, 3, 2, 2, 4, 2, 6, 12}
+*/
+func TestRemoveAllInstancesOfInts_2(t *testing.T) {
+	input := []int{1, 2, 3, 2, 2, 4, 2, 6, 12}
+	input2 := []int{16}
+	expected := []int{1, 2, 3, 2, 2, 4, 2, 6, 12}
+	actual := RemoveAllInstancesOfInts(input, input2...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfInts_3
+	input := []int{}
+	input2 := []int{}
+	expected: Length of 0
+*/
+func TestRemoveAllInstancesOfInts_3(t *testing.T) {
+	input := []int{}
+	input2 := []int{}
+	actual := RemoveAllInstancesOfInts(input, input2...)
+	expectedStr := "Length of 0"
+	if len(actual) != 0 {
+		t.Error(formatErrorTwoInputs(input, input2, expectedStr, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfFloat64s
+	input := []int{1, 2, 3, 2, 2, 4, 2, 6, 12}
+	input2 := []int{2, 1}
+	expected := []int{3, 4, 6, 12}
+*/
+func TestRemoveAllInstancesOfFloat64s_1(t *testing.T) {
+	input := []float64{2, 2, 2}
+	input2 := []float64{2, 2}
+	expected := []float64{}
+	actual := RemoveAllInstancesOfFloat64s(input, input2...)
+
+	if len(actual) != 0 {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfFloat64s_2
+	input := []float64{1.123, 2.53, 3.0000000000001, 3, 2, 4, 2, 6, 12}
+	input2 := []float64{3}
+	expected := []float64{1.123, 2.53, 3.0000000000001, 2, 4, 2, 6, 12}
+*/
+func TestRemoveAllInstancesOfFloat64s_2(t *testing.T) {
+	input := []float64{1.123, 2.53, 3.0000000000001, 3, 2, 4, 2, 6, 12}
+	input2 := []float64{3}
+	expected := []float64{1.123, 2.53, 3.0000000000001, 2, 4, 2, 6, 12}
+	actual := RemoveAllInstancesOfFloat64s(input, input2...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestRemoveAllInstancesOfFloat64s_3
+	input := []float64{1.123, 2.53, 3.0000000000001, 3, 2, 4, 2, 6, 12}
+	input2 := []float64{3}
+	expected := []float64{1.123, 2.53, 3.0000000000000, 2, 4, 2, 6, 12}
+*/
+func TestRemoveAllInstancesOfFloat64s_3(t *testing.T) {
+	input := []float64{1.123, 2.53, 3.0000000000000, 3, 2, 4, 2, 6, 12}
+	input2 := []float64{3}
+	expected := []float64{1.123, 2.53, 2, 4, 2, 6, 12}
+	actual := RemoveAllInstancesOfFloat64s(input, input2...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestContainsString_1
+	input := []string{"one", "too", "tree"}
+	input2 := "too"
+	expected := true
+*/
+func TestContainsString_1(t *testing.T) {
+	input := []string{"one", "too", "tree"}
+	input2 := "too"
+	expected := true
+	actual := ContainsString(input, input2)
+
+	if actual != expected {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestContainsString_2
+	input := []string{"one", "too", "tree"}
+	input2 := "two"
+	expected := false
+*/
+func TestContainsString_2(t *testing.T) {
+	input := []string{"one", "too", "tree"}
+	input2 := "two"
+	expected := false
+	actual := ContainsString(input, input2)
+
+	if actual != expected {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestContainsInt_1
+	input := []int{1, 2, 6}
+	input2 := 1
+	expected := true
+*/
+func TestContainsInt_1(t *testing.T) {
+	input := []int{1, 2, 6}
+	input2 := 1
+	expected := true
+	actual := ContainsInt(input, input2)
+
+	if actual != expected {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestContainsInt_2
+	input := []int{1, 2, 6}
+	input2 := 3
+	expected := false
+*/
+func TestContainsInt_2(t *testing.T) {
+	input := []int{1, 2, 6}
+	input2 := 3
+	expected := false
+	actual := ContainsInt(input, input2)
+
+	if actual != expected {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestContainsFloat64_1
+	input := []float64{1.00000, 2, 6}
+	input2 := 1.0
+	expected := true
+*/
+func TestContainsFloat64_1(t *testing.T) {
+	input := []float64{1.00000, 2, 6}
+	input2 := 1.0
+	expected := true
+	actual := ContainsFloat64(input, input2)
+
+	if actual != expected {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
+/*
+	TestContainsFloat64_2
+	input := []int{1.00000000001, 2, 6}
+	input2 := 1.0
+	expected := false
+*/
+func TestContainsFloat64_2(t *testing.T) {
+	input := []float64{1.00000000001, 2, 6}
+	input2 := 1.0
+	expected := false
+	actual := ContainsFloat64(input, input2)
+
+	if actual != expected {
+		t.Error(formatErrorTwoInputs(input, input2, expected, actual))
+	}
+}
+
 func formatError(input interface{}, expected interface{}, actual interface{}) string {
 	return fmt.Sprintf("\nInput:    %v\nExpected: %v\nActual:   %v\n", input, expected, actual)
 }
 func formatErrorWithErr(input interface{}, expected interface{}, actual interface{}, err interface{}) string {
 	return fmt.Sprintf("\nInput:    %v\nExpected: %v\nActual:   %v\nError:    %v\n", input, expected, actual, err)
+}
+func formatErrorTwoInputs(input interface{}, input2 interface{}, expected interface{}, actual interface{}) string {
+	return fmt.Sprintf("\nInput:    %v\nInput2:   %v\nExpected: %v\nActual:   %v\n", input, input2, expected, actual)
 }
